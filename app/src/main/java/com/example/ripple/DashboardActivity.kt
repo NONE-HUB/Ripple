c7103cc60045f2cf42a9033ee71b7cf406ee23e9
NO-ONE <mergichadmergi@gmail.com>
2025-12-11 14:17:04 +0545

    process of adding screen to the dashboard activity

--- app/src/main/java/com/example/ripple/DashboardActivity.kt
+++ app/src/main/java/com/example/ripple/DashboardActivity.kt
@@ -1,30 +1,157 @@
 package com.example.ripple
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+import com.example.ripple.ui.theme.RippleTheme
 
 class DashboardActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Dashboard()
+            DashboardBody()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Dashboard(){
-    Text("Hello world")
+fun DashboardBody() {
+
+    val context = LocalContext.current
+    val activity = context as? Activity
+
+    val email = activity?.intent?.getStringExtra("email") ?: ""
+    val password = activity?.intent?.getStringExtra("password") ?: ""
+
+    val navBarHeight = 110.dp
+
+    data class NavItem(val label: String, val icon: Int, val iconsize: Dp = 25.dp)
+
+    var selectedItem by remember { mutableStateOf(0) }
+
+    var navList = listOf(
+        NavItem("Home", R.drawable.home),
+        NavItem("Create", R.drawable.create),
+        NavItem("Profile", R.drawable.circle_regular_full),
+        NavItem("Notification", R.drawable.notification),
+        NavItem("Setting", R.drawable.setting),
+    )
+
+//    for(int i = 0;i<navList.size;i++){
+//        navlist[i].label
+//        navList[i].icon
+//    }
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Blue,
+                    titleContentColor = Color.White,
+                    navigationIconContentColor = Color.White,
+                    actionIconContentColor = Color.White
+                ),
+                title = { Text("Ecommerce") },
+                navigationIcon = {
+                    IconButton(onClick = {
+
+                        activity?.finish()
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.messagebox),
+                            contentDescription = null
+                        )
+
+                    }
+                },
+                actions = {
+                    IconButton(onClick = {
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.messagebox),
+                            contentDescription = null
+                        )
+                    }
+                    IconButton(onClick = {
+                    }) {
+                        Icon(
+                            painter = painterResource(R.drawable.locker),
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            NavigationBar(
+                modifier = Modifier.height(navBarHeight), // reduces overall height
+                tonalElevation = 0.dp // optional: remove shadow/elevation
+            ) {
+                navList.forEachIndexed { index, item ->
+                    NavigationBarItem(
+                        icon = {
+                            Icon(painter = painterResource(item.icon),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(32.dp)
+                                    .padding(top=5.dp))
+                        },
+                        label = {Text(item.label,
+                            fontSize = 9.sp,
+                            maxLines = 1,
+                            softWrap = false,
+                            modifier = Modifier
+                                .padding(top=0.dp,bottom=5.dp))},
+                        onClick = {
+                            selectedItem = index
+                        },
+                        selected = selectedItem == index
+                    )
+                }
+            }
+        }
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) 
+    }
 }
 
 @Preview
 @Composable
-fun DashboardPreview(){
-    Dashboard()
+fun DashboardActivityPreview(){
+    DashboardBody()
 }
