4aa63517a1fabaad70db05a37363f1d4b26cc9b3
NO-ONE <mergichadmergi@gmail.com>
2025-12-22 20:26:41 +0545

    fronend wokr

--- app/src/main/java/com/example/ripple/HomeScreen.kt
+++ app/src/main/java/com/example/ripple/HomeScreen.kt
@@ -3,10 +3,12 @@ package com.example.ripple
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 
 
 @Composable
@@ -18,6 +20,41 @@ fun HomeScreen() {
     ) {
         Text("Home Screen")
         Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+        Text("Home Screen")
+        Text("Home Screen baik aru")
+        Icon(painter = painterResource(R.drawable.eye_open), contentDescription = null)
+
     }
 }
 
