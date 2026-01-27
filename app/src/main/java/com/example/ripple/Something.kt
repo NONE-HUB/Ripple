e1a254d727a579832d574b6b8631349296880b30
manjilthapa0777 <manjilt751@gmail.com>
2026-01-27 20:37:37 +0545

    codes changed

--- app/src/main/java/com/example/ripple/Something.kt
+++ app/src/main/java/com/example/ripple/Something.kt
@@ -484,6 +484,18 @@ fun Greeting() {
 
     Spacer(modifier = Modifier .height(10.dp))
 
+    Text("Enter your username",
+        style = TextStyle(
+            fontSize = 20.sp
+        ),
+        modifier = Modifier
+            .padding(horizontal = 20.dp))
+
+    Spacer(modifier = Modifier .height(10.dp))
+
+    Spacer(modifier = Modifier .height(10.dp))
+
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -494,15 +506,7 @@ fun Greeting() {
         horizontalArrangement = Arrangement.Center
     )
 
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .size(105.dp)
-            .clip(CircleShape)
-            .padding(4.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Center
-    ){
+    {
         Image(painter = painterResource(R.drawable.instagram), contentDescription = null,
             modifier = Modifier
                 .size(95.dp)
